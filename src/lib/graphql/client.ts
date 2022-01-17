2922f1d879fee18c2af309f94bd73126f3614f12
fezproof <26830309+fezproof@users.noreply.github.com>
2022-01-18 02:39:39 +0800

    Fixed build

--- src/lib/graphql/client.ts
+++ src/lib/graphql/client.ts
@@ -5,32 +5,35 @@ import { cacheExchange } from '@urql/exchange-graphcache';
 import { retryExchange } from '@urql/exchange-retry';
 import { createClient, dedupExchange, fetchExchange } from '@urql/svelte';
 
+const exchanges = [
+	dedupExchange,
+	cacheExchange({
+		schema,
+		resolvers: {
+			Query: {
+				location: (_, args) => ({
+					__typename: 'Location',
+					id: args.id
+				})
+			}
+		},
+		keys: {
+			Game: () => null,
+			Rank: () => null,
+			Account: ({ username }) => username as string
+		}
+	}),
+	retryExchange({}),
+	fetchExchange
+];
+
+if (dev) exchanges.unshift(devtoolsExchange);
+
 export const client = createClient({
 	url: '/graphql',
 	fetchOptions: {
 		credentials: 'same-origin'
 	},
 	requestPolicy: 'cache-first',
-	exchanges: [
-		dev && devtoolsExchange,
-		dedupExchange,
-		cacheExchange({
-			schema,
-			resolvers: {
-				Query: {
-					location: (_, args) => ({
-						__typename: 'Location',
-						id: args.id
-					})
-				}
-			},
-			keys: {
-				Game: () => null,
-				Rank: () => null,
-				Account: ({ username }) => username as string
-			}
-		}),
-		retryExchange({}),
-		fetchExchange
-	]
+	exchanges
 });
