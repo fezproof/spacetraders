858a767aec7ab0e67fee0c04b8e6dd03afa8ae56
fezproof <26830309+fezproof@users.noreply.github.com>
2022-01-18 14:13:26 +0800

    Fixed position calculations

--- src/lib/utils/position.ts
+++ src/lib/utils/position.ts
@@ -3,7 +3,7 @@ import type { Position } from 'svelte-cubed';
 export const isPosition = (pos: unknown[]): pos is Position => {
 	if (pos?.length !== 3) return false;
 	for (const value of pos) {
-		if (Number.isNaN(value)) return false;
+		if (Number.isNaN(+value)) return false;
 	}
 
 	return true;
